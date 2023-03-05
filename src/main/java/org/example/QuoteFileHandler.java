f2bacd204d32fd41fafbe85250cac3d3b6ec262c
RoqayaAlmamari <roqayaalmamari@gmail.com>
2023-03-05 00:54:57 -0800

    Quote of the day

--- src/main/java/org/example/QuoteFileHandler.java
+++ src/main/java/org/example/QuoteFileHandler.java
@@ -8,8 +8,10 @@ import java.io.IOException;
 
 public class QuoteFileHandler {
 
+    // Define the path for the file
     private static final String FILE_PATH = "quote.json";
 
+    // Method to write a JSON string to the file
     public static void write(String jsonString) {
         try (FileWriter writer = new FileWriter(new File(FILE_PATH))) {
             writer.write(jsonString);
@@ -18,13 +20,16 @@ public class QuoteFileHandler {
         }
     }
 
+    // Method to read the JSON string from the file
     public static String read() {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File(FILE_PATH)))) {
             StringBuilder stringBuilder = new StringBuilder();
             String line;
+            // Read each line from the file and append it to the StringBuilder
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
             }
+            // Return the JSON string as a String
             return stringBuilder.toString();
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
